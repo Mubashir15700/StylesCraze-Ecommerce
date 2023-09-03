f70425b7f491bad1982a6acf1f965c7b0f4f66a9
Mubashir <mubashir15700@gmail.com>
2023-09-03 23:19:12 +0530

    admin side UI changes

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,8 +1,40 @@
 
 export const getLogin = (req, res) => {
     res.render('admin/login', { commonError: "" });
-}
+};
 
 export const getDashboard = (req, res) => {
     res.render('admin/dashboard');
-}
+};
+
+export const getNotifications = (req, res) => {
+    res.render('admin/notifications');
+};
+
+export const getProfile = (req, res) => {
+    res.render('admin/profile');
+};
+
+export const getOrders = (req, res) => {
+    res.render('admin/orders');
+};
+
+export const getProducts = (req, res) => {
+    res.render('admin/products');
+};
+
+export const getCategories = (req, res) => {
+    res.render('admin/categories');
+};
+
+export const getCustomers = (req, res) => {
+    res.render('admin/customers');
+};
+
+export const getSalesReport = (req, res) => {
+    res.render('admin/salesReport');
+};
+
+export const getBanner = (req, res) => {
+    res.render('admin/banner');
+};
--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,12 +1,20 @@
 import { Router } from 'express';
 import { loginAdmin, logoutAdmin } from '../controllers/authController.js';
-import { getLogin, getDashboard } from '../controllers/adminController.js';
+import { getLogin, getDashboard, getNotifications, getProfile, getOrders, getProducts, getCategories, getCustomers, getSalesReport, getBanner } from '../controllers/adminController.js';
 import { checkAuth, isLoggedIn } from '../middlewares/adminMiddleware.js';
 
 const router = Router();
 
 router.get("/", checkAuth, getDashboard);
 router.route("/login").get(isLoggedIn, getLogin).post(loginAdmin);
+router.get("/notifications", checkAuth, getNotifications);
+router.get("/profile", checkAuth, getProfile);
+router.get("/orders", checkAuth, getOrders);
+router.get("/products", checkAuth, getProducts);
+router.get("/categories", checkAuth, getCategories);
+router.get("/customers", checkAuth, getCustomers);
+router.get("/sales-report", checkAuth, getSalesReport);
+router.get("/banner", checkAuth, getBanner);
 router.post("/logout", logoutAdmin);
 
 export default router;
