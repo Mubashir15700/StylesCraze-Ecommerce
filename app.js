6c748a55e3325dc2e1d63c0c898fa3da65cd1cf4
Mubashir15700 <mubashir15700@gmail.com>
2024-06-20 12:52:56 +0530

    fix: update rate limitter to 500 requests

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use(cookieParser());
 // Rate limiter
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // limit each IP to 100 requests per windowMs
+    max: 500, // limit each IP to 100 requests per windowMs
 });
 app.use(limiter);
 
